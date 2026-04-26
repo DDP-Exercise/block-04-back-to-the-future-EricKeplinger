100bbaf83fdc087c21b3e4426921bb0eb24876ae
Ricki2001 <s2510892043@students.fh-hagenberg.at>
2026-04-26 18:34:02 +0200

    format the time so the numbers from 0-9 get an extra 0 in front of them

--- js/view.digital.js
+++ js/view.digital.js
@@ -3,7 +3,19 @@
 export let digitalClock = {
     updateClock(hours, minutes, seconds){
         let digital = document.getElementById("digital");
+
+        if (hours < 10) {
+            hours = "0" + hours;
+        }
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        }
+        if (seconds < 10) {
+            seconds = "0" + seconds;
+        }
+
         digital.innerText = hours + ":" + minutes + ":" + seconds;
+
 
     }
 }
